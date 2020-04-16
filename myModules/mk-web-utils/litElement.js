09528b3d907e852f80d54e801316b92c2fc63703
Michał Kownacki <mk@michalkownacki.pl>
2020-04-16 18:29:20 +0200

    Add option to add attributes in dynamicElement.

--- myModules/mk-web-utils/litElement.js
+++ myModules/mk-web-utils/litElement.js
@@ -1,3 +1,5 @@
+import {mapValuesAndKeys} from 'mk-utils/general.js';
+
 //todo How about GC
 const memoizedParse =_.memoize(JSON.parse);
 export const staticProp = (object) => {
@@ -6,10 +8,11 @@ export const staticProp = (object) => {
 
 //todo Fix unintentional caching results just like cache directive. Idea: Create own directive.
 const dynamicElements = new WeakMap;
-export const dynamicElement = (host, key, tagName, props) => {
+export const dynamicElement = (host, key, tagName, props, attrs) => {
   const dynamicElementsOfHost = dynamicElements.get(host) || new Map;
   dynamicElements.set(host, dynamicElementsOfHost);
   const element = dynamicElementsOfHost.get(key) || document.createElement(tagName);
   dynamicElementsOfHost.set(key, element);
+  mapValuesAndKeys(_, (attrValue, attrName) => element.setAttribute(attrName, attrValue), attrs);
   return Object.assign(element, props);
 };
