e158edc9e5c18b68155a746761e31a02f9e985cd
Michał Kownacki <mk@michalkownacki.pl>
2020-04-20 23:50:43 +0200

    Fix bug when generating index.

--- utils/generateIndex/preRender.js
+++ utils/generateIndex/preRender.js
@@ -2,7 +2,7 @@ import loadingDots from 'mk-web-utils/loadingDots.js'
 
 export default `
   <style>
-    ${loadingDots.css}
+    ${loadingDots.css()}
   </style>
   ${loadingDots.html(`<noscript>Enabled JavaScript is required to view the page.</noscript>`)}
 `;
