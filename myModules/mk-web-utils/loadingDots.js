cc22dc8b726c6cfdaa6bef8fa66fc3cdb8ccd154
Michał Kownacki <mk@michalkownacki.pl>
2020-04-20 20:46:57 +0200

    Move loading dots snippet to mk-web-utils.

---
+++ myModules/mk-web-utils/loadingDots.js
@@ -0,0 +1,52 @@
+export default {
+  css: `
+    .loading {
+      font-family: sans-serif;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+    .loading-dots {
+      font-size: 50px;
+    }
+    noscript {
+      text-align: center;
+      margin: 10px;
+    }
+    @keyframes blink {
+      0% {
+        opacity: .2;
+      }
+      20% {
+        opacity: 1;
+      }
+      100% {
+        opacity: .2;
+      }
+    }
+    .loading span {
+      animation-name: blink;
+      animation-duration: 1.4s;
+      animation-iteration-count: infinite;
+      animation-fill-mode: both;
+    }
+    .loading span:nth-child(2) {
+      animation-delay: .2s;
+    }
+    .loading span:nth-child(3) {
+      animation-delay: .4s;
+    }
+  `,
+  html: (slot = '') => `
+    <div class="loading">
+      <div class="loading-dots"><span>•</span><span>•</span><span>•</span></div>
+      ${slot}
+    </div>
+  `,
+}
