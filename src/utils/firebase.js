ae466a4a3e050ae40641ace16b3c7cfb9a254fd6
Michał Kownacki <mk@michalkownacki.pl>
2022-03-08 12:38:17 +0100

    Use updated mk-firebase-utils.

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,3 +1,3 @@
-import {firebaseUtils} from 'mk-firebase-utils';
+import {mkFirebaseUtils} from 'mk-firebase-utils';
 
-export default firebaseUtils(_, firebase.firestore(), firebase.storage());
+export default mkFirebaseUtils(_, firebase.firestore(), firebase.storage());
